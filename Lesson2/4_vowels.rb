a021bb470e2a21a3860036bb000a66eb523cfdf3
snoopsD <snoops@list.ru>
2017-05-19 10:16:18 +0400

    Add files via upload

--- Lesson2/4_vowels.rb
+++ Lesson2/4_vowels.rb
@@ -3,14 +3,14 @@
 
 vowels = %w[а у о ы и э я ю е]
 alphabet = ('а'..'я').to_a
-
-h_vowels = Hash.new
+h_vowels = {}
 
 i = 0
 
+#как я понимаю в руби у русского алфавита 32 буквы. Е и Ё одиннаковые для него.
 vowels.each {|letter| if alphabet.include?letter
-   h_vowels = { "#{vowels[i]}" => "#{alphabet.index(letter)+1}".to_i}
+   h_vowels["#{vowels[i]}"] = "#{alphabet.index(letter)+1}"
    i += 1
  end
 }
- puts h_vowels
+puts h_vowels
